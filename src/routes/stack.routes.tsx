66126c0835bd3974e61f7e7a2e5c266e20d080f0
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-06-15 20:50:40 -0300

    Registrando página de cadastro de clientes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Client } from '../pages/Client';
+import { RegisterClient } from '../pages/RegisterClient';
 
 export function StackNavigation() {
   const { Navigator, Screen } = createStackNavigator();
@@ -9,6 +10,7 @@ export function StackNavigation() {
   return (
     <Navigator headerMode="none">
       <Screen name="Client" component={Client} />
+      <Screen name="RegisterClient" component={RegisterClient} />
     </Navigator>
   );
 }
