18b8b1690988950d1ba309619eb6e408a4e21d3c
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-06-16 21:29:44 -0300

    Função para remover mascára de dinheiro

--- src/utils/mask.ts
+++ src/utils/mask.ts
@@ -2,3 +2,8 @@ export function unmaskCpf(cpf: string): number {
   const result = cpf.replace(/\.|-/gm, '');
   return Number(result);
 }
+
+export function unmaskmMoney(balance: string): number {
+  const result = balance.replace(/[^0-9,]*/g, '').replace(',', '.');
+  return Number(result);
+}
